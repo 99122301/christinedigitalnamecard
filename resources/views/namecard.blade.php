92f9053f4eb9c93fb6870faf90dae530717eb313
Christine Ho <christine.ho@1cube.my>
2019-12-06 01:38:36 +0800

    digital-name-card

---
+++ resources/views/namecard.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+ 	<link rel="stylesheet" href="namecardassets/main.css">
+ 	<link href='https://fonts.googleapis.com/css?family=Lato:200,300,400,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Muli:200,300" rel="stylesheet">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<body>
+	<section class="bg-color-green banner">
+		<div class="container">
+			<div class="row">
+				<div class="col"></div>
+				<div class="col is-5">
+					<img class="profile-pic" src="namecardassets/christine.jpg" alt="Profile Picture">
+				</div>
+				<div class="col"></div>
+			</div>
+		</div>
+	</section>
+
+	<section class="bg-color-white primary-info">
+		<div class="container">
+			<div class="row name-title-company">
+				<div class="col is-6">
+					<p class="text-name">Christine Ho</p>
+					<p class="text-title-company">Computing Intern</p>
+					<p class="text-title-company">Cube Solutions</p>
+					<p><q cite="https://www.imdb.com/title/tt0062622/quotes/qt0396921">Feeling cool with digitalized namecards.</q></p>
+				</div>
+				<div class="col">
+					<img src="namecardassets/assets/dog.jpg" alt="Cute doggo" >
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="bg-color-white secondary-info">
+		<a href="mailto:christine.ho@1cube.my">
+			<div class="container">
+				<div class="row detail">
+					<div class="col">
+						<img src="namecardassets/assets/icon-email.svg" alt="Email Icon"">
+					</div>
+					<div class="col is-8">
+						<p class="text-detail">christine.ho@1cube.my</p>
+					</div>
+					<div class="col">
+						<img src="namecardassets/assets/right-arrow.svg" alt="Right Arrow" height="50%" width="40%">
+					</div>
+				</div>
+			</div>
+		</a>
+	</section>
+
+	<section class="bg-color-white secondary-info">
+		<a href="tel:+601161179780">
+			<div class="container">
+				<div class="row detail">
+					<div class="col">
+						<img src="namecardassets/assets/icon-phone.svg" alt="Phone Icon"">
+					</div>
+					<div class="col is-8">
+						<p class="text-detail">+6011 6117 9780</p>
+					</div>
+					<div class="col">
+						<img src="namecardassets/assets/right-arrow.svg" alt="Right Arrow" height="50%" width="40%">
+					</div>
+				</div>
+			</div>
+		</a>
+	</section>
+
+	<section class="bg-color-white secondary-info">
+		<a href="https://1cube.my">
+			<div class="container">
+				<div class="row detail">
+					<div class="col">
+						<img src="namecardassets/assets/icon-website.svg" alt="Website Icon"">
+					</div>
+					<div class="col is-8">
+						<p class="text-detail">1cube.my</p>
+					</div>
+					<div class="col">
+						<img src="namecardassets/assets/right-arrow.svg" alt="Right Arrow" height="50%" width="40%">
+					</div>
+				</div>
+			</div>
+		</a>
+	</section>
+
+	<section class="bg-color-white secondary-info">
+		<a href="https://www.google.com/maps/place/Plaza+Jelutong,+5c,+Persiaran+Gerbang+Utama,+Bukit+Jelutong,+40150+Shah+Alam,+Selangor/data=!4m2!3m1!1s0x31cc4e00c6b2b5e7:0x187ec3993b372a07?sa=X&ved=2ahUKEwji-MPBy57mAhXEXSsKHdJdBk8Q8gEwAHoECAsQAQ">
+			<div class="container">
+				<div class="row detail">
+					<div class="col">
+						<img src="namecardassets/assets/icon-address.svg" alt="Address Icon"">
+					</div>
+					<div class="col is-8">
+						<p class="text-detail">5C, Persiaran Gerbang Utama, Plaza Jelutong Bukit Jelutong
+Shah Alam, Selangor 40150
+Malaysia</p>
+					</div>
+					<div class="col">
+						<img src="namecardassets/assets/right-arrow.svg" alt="Right Arrow" height="50%" width="40%">
+					</div>
+				</div>
+			</div>
+		</a>
+	</section>
+
+	<section class="bg-color-white bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col"></div>
+				<div class="col is-5">
+					<div class="button">
+						<a href="namecardassets/christine.vcf">
+							<p class="button">Download my VCF</p>
+						</a>
+					</div>
+				</div>
+				<div class="col"></div>
+			</div>
+		</div>
+	</section>
+
+</body>
+
+</html>
